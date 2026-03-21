ef8e80b6a1d8536474b03be954f2d71e931ff1fd
ffuT <88716247+ffuT@users.noreply.github.com>
2026-03-21 15:29:59 +0100

    variable jump height

--- src/models/player.lua
+++ src/models/player.lua
@@ -2,6 +2,8 @@ local physics = require("src.models.physics")
 
 ---@class Player: Object
 ---@field body Body
+---@field notOnGround boolean
+---@field jumpDelay number
 local Player = {}
 Player.__index = Player
 
@@ -12,6 +14,8 @@ Player.__index = Player
 function Player.new(x, y, spriteOverwrite)
 	local self = setmetatable({}, Player)
  	self.body = physics.Body.new(x, y)
+	self.notOnGround = false
+	self.jumpDelay = 1
 	self.sprite = spriteOverwrite or Game.assets.images.playerImg
 	return self
 end
@@ -26,20 +30,31 @@ function Player:update(dt)
 
 	if (self.body.y >= GAME_HEIGHT - TILE_SIZE or self.body.y <=  0) then
 		self.body.vely = 0
+		self.notOnGround = false
+	else
+		self.notOnGround = true;
+	end
+
+	if(self.notOnGround) then
+		self.jumpDelay = self.jumpDelay - dt
+	else
+		self.jumpDelay = 0.15
 	end
 
 	-- keyboard handling --
 	if love.keyboard.isDown("w") then
-		self.body:addForce(0, -50);
+		if self.jumpDelay >= 0 then
+			self.body:addForce(0, -50);
+		end
 	end
 	if love.keyboard.isDown("s") then
-		self.body:addForce(0, 5);
+		self.body:addForce(0, 3);
 	end
 	if love.keyboard.isDown("d") then
-		self.body:addForce(5, 0);
+		self.body:addForce(3, 0);
 	end
 	if love.keyboard.isDown("a") then
-		self.body:addForce(-5, 0);
+		self.body:addForce(-3, 0);
 	end
 
 	self.body:integrate(dt)
